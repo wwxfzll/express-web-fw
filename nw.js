01dd5459bbe3b096666b67187804924394c2e173
wwxfzll <276272330@qq.com>
2020-01-18 17:27:02 +0800

    添加window服务功能

---
+++ nw.js
@@ -0,0 +1,13 @@
+﻿let Service = require('node-windows').Service
+
+let svc = new Service({
+    name: 'components_node',
+    description: '组件库网站',
+    script: 'E:/github/express-web-fw/bin/web.js'//部署在服务器对应的路径
+})
+
+svc.on('install', function() {
+    svc.start()
+})
+
+svc.install()
